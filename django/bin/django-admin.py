f0b9fca81d95b6b539ed3af53212759915f67e73
Neeky <1721900707@qq.com>
2017-06-14 13:18:03 +0800

    17.06.13 给django-admin增加注释

--- django/bin/django-admin.py
+++ django/bin/django-admin.py
@@ -2,4 +2,5 @@
 from django.core import management
 
 if __name__ == "__main__":
+    #django-admin 直接调用的django.core.management.execute_from_command_line()
     management.execute_from_command_line()
